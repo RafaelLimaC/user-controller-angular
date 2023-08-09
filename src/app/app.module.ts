9fda8fdc8fa01d93c59f4751761344ce8a3a2198
Rafael Lima <srafalimacosta@gmail.com>
2023-08-09 10:28:50 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterModalComponent } from './components/register-modal/register-modal.component';
+import { LoginModalComponent } from './components/login-modal/login-modal.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './shared/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterModalComponent,
+    LoginModalComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
