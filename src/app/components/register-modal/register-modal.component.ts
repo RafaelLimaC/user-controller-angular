a509191ed10f4f9287596e84c235fe69e7fa59ff
Rafael Lima <srafalimacosta@gmail.com>
2023-08-16 15:35:53 -0300

    <fix> implementando verificação de minlenght no campo password

--- src/app/components/register-modal/register-modal.component.ts
+++ src/app/components/register-modal/register-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, NgForm, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 
@@ -10,23 +10,19 @@ import { Router } from '@angular/router';
 })
 export class RegisterModalComponent {
 
-  constructor(public dialogRef: MatDialogRef<RegisterModalComponent>, private router: Router) {}
-
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
-  confirmPassword = new FormControl('', [Validators.required]);
+  formRegister: FormGroup;
   hide = true;
 
-  getErrorMessage(field: FormControl) {
-
-    if (field.hasError('required')) {
-      return 'Esse campo é obrigatório';
-    } else if (field.hasError('email')) {
-      return 'Email inválido';
-    }
-
-
-    return 'Erro 404';
+  constructor(
+    public dialogRef: MatDialogRef<RegisterModalComponent>, 
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.formRegister = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required])
+    })
   }
 
   register(form: NgForm) {
